c04995269dfaf93a844979e73dba4192650fc863
Minakoss <nontaspapastefanis@gmail.com>
2024-08-02 23:20:31 +0300

    import fixes & install prettier formatter

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,20 @@
-import '@styles/global.css';
-import { Children } from 'react/cjs/react.production.min';
-export const metadata={
-    title: "promptopia",
-    description: 'Discover & Share AI Prompts'
-}
+import "@styles/globals.css";
+export const metadata = {
+  title: "promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({Children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html Lang="en">
-        <body>
-            <div className="main">
-<div className="gradient"/>
-            </div>
-            <main className="app">
-                {Children}
-
-            </main>
-        </body>
-
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
